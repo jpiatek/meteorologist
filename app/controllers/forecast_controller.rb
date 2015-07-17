dde4e50564492455d0cdcfc0ff5e4df0bf1c698c
jpiatek <jpiatek@chicagobooth.edu>
2015-07-16 21:49:40 -0500

    coords--weather
    this is awesome

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -10,23 +10,27 @@ class ForecastController < ApplicationController
     @lat = params[:user_latitude]
     @lng = params[:user_longitude]
 
+
+    base_url="https://api.forecast.io/forecast/c3dc222c33812ab534d5f3c93ef6b0b2/"
+    full_url=base_url+@lat+","+@lng
+
     # ==========================================================================
     # Your code goes below.
     # The latitude the user input is in the string @lat.
     # The longitude the user input is in the string @lng.
     # ==========================================================================
 
+    parsed_data = JSON.parse(open(full_url).read)
 
+    @current_temperature = parsed_data["currently"]["temperature"]
 
-    @current_temperature = "Replace this string with your answer."
+    @current_summary = parsed_data["currently"]["summary"]
 
-    @current_summary = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = parsed_data["hourly"]["data"][0]["summary"]
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @summary_of_next_several_hours = parsed_data["hourly"]["summary"]
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
-
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_days =  parsed_data["daily"]["summary"]
 
     render("coords_to_weather.html.erb")
   end
